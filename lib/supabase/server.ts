6653b0c9d568c267c30a5ba70d5d8eb566e7b66e
Carlos Guaragna <robertogna@gmail.com>
2026-07-18 18:56:37 +0200

    Preencher os metadados do pagamento no webhook

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE_URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log(
+  "SUPABASE_KEY EXISTS =",
+  !!process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+);
+
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
